a717cf26dbc92dff74838be6279d5ae0945aea03
namhihi237 <yentth237@gmail.com>
2023-11-18 18:55:38 +0700

    fix: remove schame migration

--- src/migrations/tenanted/1700276059284-add-test-tenant.ts
+++ src/migrations/tenanted/1700276059284-add-test-tenant.ts
@@ -1,20 +1,15 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
-import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
 
 export class AddTestTenant1700276059284 implements MigrationInterface {
   name = 'AddTestTenant1700276059284';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    const { schema } = queryRunner.connection.options as PostgresConnectionOptions;
-
     await queryRunner.query(
-      `CREATE TABLE ${schema}."tests" ("id" SERIAL NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "name" character varying NOT NULL, CONSTRAINT "PK_4301ca51edf839623386860aed2" PRIMARY KEY ("id"))`,
+      `CREATE TABLE "tests" ("id" SERIAL NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "name" character varying NOT NULL, CONSTRAINT "PK_4301ca51edf839623386860aed2" PRIMARY KEY ("id"))`,
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    const { schema } = queryRunner.connection.options as PostgresConnectionOptions;
-
-    await queryRunner.query(`DROP TABLE  ${schema}."tests"`);
+    await queryRunner.query(`DROP TABLE "tests"`);
   }
 }
