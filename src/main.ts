ee7f6164914fe1a44901b68820d54247e4b75804
namhihi237 <yentth237@gmail.com>
2023-12-13 13:04:20 +0700

    fix: auto create main db

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,45 @@ import * as express from 'express';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { tenancyMiddleware } from './modules/tenancy/tenancy.middleware';
 import { seederRun } from './seeder';
+import { DataSourceOptions } from 'typeorm';
+import { SnakeNamingStrategy } from './snake-naming.strategy';
+import { join } from 'path';
+const { HOST, PORT, DB_NAME, DB_USERNAME, DB_PASSWORD } = process.env;
+import { createConnection } from 'typeorm';
 
 async function bootstrap() {
+  const connectionOptions = {
+    type: 'postgres',
+    host: HOST,
+    port: PORT,
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: 'postgres',
+    namingStrategy: new SnakeNamingStrategy(),
+    entities: [join(__dirname, './modules/public/**/*.entity{.ts,.js}')],
+    migrations: [join(__dirname, './migrations/public/*{.ts,.js}')],
+  };
+
+  const adminConnection = await createConnection(connectionOptions as DataSourceOptions);
+
+  try {
+    // Check if the database already exists
+    const databaseExists = await adminConnection.query(`SELECT 1 FROM pg_database WHERE datname = '${DB_NAME}'`);
+
+    if (!databaseExists.length) {
+      // Create a new database if it doesn't exist
+      await adminConnection.query(`CREATE DATABASE ${DB_NAME}`);
+      console.log(`Database '${DB_NAME}' created successfully.`);
+    } else {
+      console.log(`Database '${DB_NAME}' already exists.`);
+    }
+  } catch (error) {
+    console.error('Error checking/creating the database:', error);
+  } finally {
+    // Close the temporary connection
+    await adminConnection.close();
+  }
+
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     rawBody: true,
   });
