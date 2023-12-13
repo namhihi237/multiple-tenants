0590c86df23c745036a0e00c8f28a98400e12d34
namhihi237 <yentth237@gmail.com>
2023-12-13 12:22:53 +0700

    fix: create new db when create new tenant

--- src/modules/public/db-server/db-server.service.ts
+++ src/modules/public/db-server/db-server.service.ts
@@ -3,15 +3,21 @@ import { DbServer } from './db-server.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateDbDto } from './db-server.dto';
+import * as pgPromise from 'pg-promise';
 
 @Injectable()
 export class DbServerService {
+  private db: any;
   constructor(
     @InjectRepository(DbServer)
     private readonly dbsRepository: Repository<DbServer>,
-  ) {}
+  ) {
+    this.db = pgPromise();
+  }
+
   async createDb(createDbDto: CreateDbDto) {
     const db = this.dbsRepository.create(createDbDto);
+    await this.createDatabaseWithRemoteHost(createDbDto);
     return this.dbsRepository.save(db);
   }
 
@@ -25,5 +31,34 @@ export class DbServerService {
 
   getConnectionString(dbServer: DbServer): string {
     return `postgres://${dbServer.user}:${dbServer.password}@${dbServer.host}:${dbServer.port}/${dbServer.dbName}`;
+  }
+
+  async createDatabaseWithRemoteHost(serverInfo: CreateDbDto): Promise<void> {
+    const { host, port, password, user, dbName } = serverInfo;
+    // Connect to the remote database server
+    const connectionDetails = {
+      host,
+      port,
+      user,
+      password,
+      database: 'postgres', // You can use 'postgres' or 'template1' as the default database
+    };
+
+    const remoteDb = this.db(connectionDetails);
+
+    try {
+      // Create a new database
+      await remoteDb.none(`CREATE DATABASE $1:raw`, [dbName]);
+
+      // Optionally, you might want to create users, set permissions, etc.
+
+      // Close the connection to the remote database
+      await remoteDb.$pool.end();
+      console.log('Created DB succuss:', dbName);
+    } catch (error) {
+      // Handle errors (e.g., database already exists, permission issues)
+      console.error('Error creating database:', error.message);
+      throw error;
+    }
   }
 }
